d877f11cddac543135a268eb843fd13b645eb0cc
Lasen Punyawardana <lpunyawardana@gmail.com>
2025-11-20 22:32:14 +0530

    Initial Commit

---
+++ the_atlas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  the_atlas
+//
+//  Created by Lasen Punyawardana on 2025-11-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ the_atlas/the_atlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  the_atlasApp.swift
+//  the_atlas
+//
+//  Created by Lasen Punyawardana on 2025-11-20.
+//
+
+import SwiftUI
+
+@main
+struct the_atlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
